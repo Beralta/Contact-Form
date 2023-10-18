38e3ebe35d89920f13d7d67a1e8a0ef0e43787fa
beralta <pedrohugoferreira@gmail.com>
2023-10-17 21:59:29 -0300

    Create paths and the logic pages requisitions

---
+++ app/functions/pages.php
@@ -0,0 +1,14 @@
+<?php
+
+function load() {
+    $page = strip_tags($_GET['page']);
+
+    $page = (!$page) ? 'pages/home.php' : "pages/{$page}.php";    
+
+    if(!file_exists($page)) {
+        throw new Exception("Error ");
+        
+    }
+    return $page;
+}
+
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php require('../app/functions/pages.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.min.js" integrity="sha512-WW8/jxkELe2CAiE4LvQfwm1rajOS8PHasCCx+knHG0gBHt8EXxS6T6tJRTGuDQVnluuAvMxWF4j8SNFDKceLFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <?php require load(); ?>
+    </div>
+</body>
+</html>
